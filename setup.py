ab32e82b701452a650db6c6e15516d0459af388c
Alex Morega <alex@grep.ro>
2010-07-29 15:46:23 +0300

    basic scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='edw.test',
+      version='0.1',
+      author='Eau de Web',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[]
+)
