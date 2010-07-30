af155f3b730614394e6926789360055d0b0823d0
Alex Morega <alex@grep.ro>
2010-07-30 13:21:47 +0300

    hook into buildout

--- edw/test/run.py
+++ edw/test/run.py
@@ -1,3 +1,9 @@
+import sys
+import os
+from os import path
+from contextlib import contextmanager
+from tempfile import mkstemp
+
 import transaction
 
 def wsgireffix(app):
@@ -11,13 +17,20 @@ def wsgireffix(app):
 
     return wrapper
 
-def main():
-    app = make_wsgi_app(install_fixtures)
+def quickserver(app):
     from wsgiref.simple_server import make_server
+    print "waiting for requests"
     make_server('127.0.0.1', 8080, wsgireffix(app)).serve_forever()
 
-def make_wsgi_app(install_fixtures):
-    config_file_path = make_instance()
+def main(part_name=None):
+    assert part_name is not None, "Please specify the name of a buildout part"
+
+    with zope_config(part_name) as config_file_path:
+        app = make_wsgi_app(config_file_path, install_fixtures)
+
+    quickserver(app)
+
+def make_wsgi_app(config_file_path, install_fixtures):
     from App.config import setConfiguration
     from Zope2.Startup import get_starter
     from Zope2.Startup.handlers import handleConfig
@@ -34,10 +47,10 @@ def make_wsgi_app(install_fixtures):
     starter.prepare()
 
     base_db = opts.configroot.dbtab.getDatabase('/')
-    install_fixtures(base_db)
+    #install_fixtures(base_db)
 
     from ZODB.DemoStorage import DemoStorage
-    demo_storage = ZODB.DemoStorage.DemoStorage(base=base_db)
+    demo_storage = DemoStorage(base=base_db)
 
     import Zope2
     Zope2._stuff = (demo_storage, 'Application')
@@ -51,34 +64,33 @@ def install_fixtures(db):
     from Products.Naaya.NySite import manage_addNySite
     print "naaya!"
 
-def make_instance():
-    p = "/Work/repos/edw.test/sandbox26/var/zope_instance_temp"
-    p_conf = p + '/etc/zope.conf'
-    import os
-    if os.path.isdir(p):
-        import shutil
-        shutil.rmtree(p)
-    os.mkdir(p)
-    os.mkdir(p + '/etc')
-    with open(p_conf, 'wb') as f:
-        f.write(zope_conf_tmpl % {'ih_path': p})
-    return p_conf
+@contextmanager
+def zope_config(part_name):
+    buildout_root = path.dirname(path.dirname(sys.argv[0]))
+    part_script = path.join(buildout_root, 'bin', part_name)
 
-zope_conf_tmpl = """
-%%define INSTANCE %(ih_path)s
-instancehome $INSTANCE
+    with open(part_script, 'rb') as f:
+        all_script = f.read()
+        end = 'import plone.recipe.zope2instance.ctl'
+        script = all_script[:all_script.index(end)]
+    exec script
 
-<zodb_db main>
-    <mappingstorage>
-    </mappingstorage>
-    mount-point /
-</zodb_db>
+    zope_conf_path = path.join(buildout_root, 'parts', part_name,
+                               'etc', 'zope.conf')
+    yield zope_conf_path
 
-<zodb_db temporary>
-    <temporarystorage>
-      name temporary storage for sessioning
-    </temporarystorage>
-    mount-point /temp_folder
-    container-class Products.TemporaryFolder.TemporaryContainer
-</zodb_db>
-"""
+    #test_db = """
+    #<zodb_db main>
+    #    <mappingstorage>
+    #    </mappingstorage>
+    #    mount-point /
+    #</zodb_db>
+    #"""
+
+    #fd, config_file_path = mkstemp()
+    #with os.fdopen(fd, 'wb') as config_file:
+    #    config_file.write(something)
+
+    #yield config_file_path
+
+    #os.unlink(config_file_path)
